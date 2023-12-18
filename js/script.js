ec68da2f00911d923658fab655a25178e4d16714
AimanFadil <aimanbello2001@gmail.com>
2023-12-18 16:04:15 +0100

    "FINE MILESTONE "N°2 MILESTONE"

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ createApp({
                 {
                     name: 'Fabio',
                     avatar: './img/avatar_2.jpg',
-                    visible: false,
+                    visible: true,
                     messages: [
                         {
                             date: '20/03/2020 16:30:00',
@@ -54,7 +54,7 @@ createApp({
                 {
                     name: 'Samuele',
                     avatar: './img/avatar_3.jpg',
-                    visible: false,
+                    visible: true,
                     messages: [
                         {
                             date: '28/03/2020 10:10:40',
@@ -76,7 +76,7 @@ createApp({
                 {
                     name: 'Alessandro B.',
                     avatar: './img/avatar_4.jpg',
-                    visible: false,
+                    visible: true,
                     messages: [
                         {
                             date: '10/01/2020 15:30:55',
@@ -93,7 +93,7 @@ createApp({
                 {
                     name: 'Alessandro L.',
                     avatar: './img/avatar_5.jpg',
-                    visible: false,
+                    visible: true,
                     messages: [
                         {
                             date: '10/01/2020 15:30:55',
@@ -110,7 +110,7 @@ createApp({
                 {
                     name: 'Claudia',
                     avatar: './img/avatar_6.jpg',
-                    visible: false,
+                    visible: true,
                     messages: [
                         {
                             date: '10/01/2020 15:30:55',
@@ -132,7 +132,7 @@ createApp({
                 {
                     name: 'Federico',
                     avatar: './img/avatar_7.jpg',
-                    visible: false,
+                    visible: true,
                     messages: [
                         {
                             date: '10/01/2020 15:30:55',
@@ -149,7 +149,7 @@ createApp({
                 {
                     name: 'Davide',
                     avatar: './img/avatar_8.jpg',
-                    visible: false,
+                    visible: true,
                     messages: [
                         {
                             date: '10/01/2020 15:30:55',
@@ -173,9 +173,11 @@ createApp({
        } 
     },
     methods:{
-        showChat(contatti) {
-            this.activeChat = contatti;
+        showChat: function (index) {
+            this.activeChat = index;
+            console.log(index)
         }
+       
 
        
       
